265014792ad80e92ffc94d0a47f69d1299b7c73d
Bohangood <denisbohan3@gmail.com>
2025-10-04 20:33:34 +0200

    ff

---
+++ lekce11/main.py
@@ -0,0 +1,16 @@
+#магические методи
+class Num:
+    def __init__(self, name, num2):
+        self.name = name
+        self.num2 = num2
+    
+    # def __add__(self, other):
+    #     return self.num2 + other.num2
+    
+    def __sub__(self, other):
+        return (self.num2 - other.num2)
+    
+s1 = Num("Alex", 15)
+s2 = Num("Marie", 25)
+
+print(s1 - s2)
